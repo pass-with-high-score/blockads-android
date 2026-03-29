18ea578d4baedbe0f31ea7394e60089126d05a13
Nguyen Quang Minh (NQM) <nguyenquangminh391@gmail.com>
2026-03-29 08:44:24 +0700

    fix missing folder

--- scripts/build_tunnel.sh
+++ scripts/build_tunnel.sh
@@ -5,9 +5,8 @@ echo "Building Go tunnel for Android with 16KB page size support..."
 
 cd "$(dirname "$0")/../tunnel"
 
-# -trimpath removes absolute paths from the binary
-# -ldflags='-s -w' strips debug information and symbol tables
-# -extldflags=-Wl,-z,max-page-size=16384 enables Android 15 16KB page size support
+mkdir -p ../app/libs
+
 gomobile bind -target=android -androidapi 24 -trimpath -ldflags='-s -w -extldflags=-Wl,-z,max-page-size=16384' -o ../app/libs/tunnel.aar .
 
 echo "Build complete! The AAR and JAR have been saved to app/libs/"
